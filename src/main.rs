e2444570c16cd7d5275f7f4c55c358e888d0d9de
SwimmingPigLuvr <dnevell95@gmail.com>
2023-04-02 23:13:04 -0700

    casiopea

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+use std::time::Instant;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("__getGoodWithNumbers__");
+    println!("======================");
+    println!(".  solve in ur head  .");
+}
