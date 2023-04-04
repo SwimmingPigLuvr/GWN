d1bfdbcccd099805dd066d6671ba6ec54ff19067
SwimmingPigLuvr <dnevell95@gmail.com>
2023-04-04 07:48:11 -0700

    made sure num1 > num2

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,78 @@
-use std::io::{self, Read};
-use std::time::Instant;
+use std::io;
+use std::time::{Instant, Duration};
 use rand::Rng;
+
+// todo
+// only non negatie subtraction answers
+// so lets sort everything a > b
 
 fn generate_math_problem() -> (String, i32) {
     let mut rng = rand::thread_rng();
-    let num1 = rng.gen_range(1..100);
-    let num2 = rng.gen_range(1..100);
-    let operation = rng.gen_range(1..5);
+    let mut num1 = rng.gen_range(1..100);
+    let mut num2 = rng.gen_range(1..100);
+    let mult1 = rng.gen_range(1..=20);
+    let mult2 = rng.gen_range(1..=20);
+    let operation = rng.gen_range(1..=3);
 
     match operation {
-        1 => (format!("{} + {}", num1, num2), num1 + num2),
-        2 => (format!("{} - {}", num1, num2), num1 - num2),
-        3 => (format!("{} * {}", num1, num2), num1 * num2),
-        4 => {
-            if num2 == 0 {
-                generate_math_problem()
-            } else {
-                (format!("{} / {}", num1, num2), num1 / num2)
+        1 => {
+            if num1 > num2 {
+                std::mem::swap(&mut num1, &mut num2);
             }
+            (format!("{} + {}", num1, num2), num1 + num2)
+        },
+        2 => {
+            if num1 > num2 {
+                std::mem::swap(&mut num1, &mut num2);
+            }
+            (format!("{} - {}", num1, num2), num1 - num2)
         }
+        3 => (format!("{} * {}", mult1, mult2), mult1 * mult2),
         _ => unreachable!(),
     }
 }
 
 fn main() {
-    println!("__getGoodWithNumbers__");
-    println!("======================");
-    println!(".  solve in ur head  .");
-
-    let time_limit = 60;
     let mut correct = 0;
     let mut total = 0;
+
+    let game_duration = Duration::from_secs(60);
     let start_time = Instant::now();
 
-    while start_time.elapsed().as_secs() < time_limit {
-        let (problem, answer) = generate_math_problem();
+    println!("You have 60 seconds to solve as many math problems as you can!");
 
-        println!("|{}|", problem);
+    while start_time.elapsed() < game_duration {
+        let (problem, answer) = generate_math_problem();
+        println!("{}", problem);
 
         let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).expect("falied to read input");
+        io::stdin().read_line(&mut user_input).expect("Failed to read input");
 
         let user_input: i32 = match user_input.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Invalid input. Please enter a number.");
+                continue;
+            }
         };
 
-        total += 1;
         if user_input == answer {
-            println!("correct");
             correct += 1;
+            println!("Correct!");
         } else {
-            println!("NO! the correct answer is |{}|", answer);
+            println!("Incorrect. The correct answer was {}.", answer);
         }
+
+        total += 1;
+
+        let time_left = game_duration.checked_sub(start_time.elapsed()).unwrap_or_else(|| Duration::new(0, 0));
+        println!("Time left: {} seconds", time_left.as_secs());
     }
 
-    println!("Done");
-    println!("{} right out of {}", correct, total);
-
+    println!(
+        "Time's up! You answered {}/{} questions correctly.",
+        correct, total
+    );
 }
+
 
